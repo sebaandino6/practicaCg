ff47c543bf0105300099732110106bef055ea4e4
renerecalde <renerecalde2@gmail.com>
2021-09-29 17:06:18 -0300

    Add rasterizacion bresenham inconplete.

--- rasterizacion/rasterizacion_v2/src/raster_algs.cpp
+++ rasterizacion/rasterizacion_v2/src/raster_algs.cpp
@@ -16,36 +16,100 @@ void draw_line(Point p0, Point p1) {
     glBegin(GL_POINTS);
        
     glColor4f(0,0,0,.5);
-    float dy = (p1.y-p0.y);
-    float dx = (p1.x-p0.x);
+    
+    //Bresenham
+        float dy = (p1.y-p0.y);
+        float dx = (p1.x-p0.x);
+        float di;
+        float m;
+        float pasoDi;
+    
     //Como se si la curva es suficientemente suave?
     
-    if(abs(dx) < abs(dy)) {
-        if(dy!=0){
+        if(abs(dx) < abs(dy)) {
+                    
             if(dy < 0){
                 std::swap(p0,p1);
             }
-                
-            float m = dx/dy;
+            
             for( Point p = p0; p.y <= p1.y; ++p.y) {
-                glVertex2f(p.x,redon(p.y));
-                p.x+= m;
-            }    
-        }    
-    }else{
-        if(dx!=0){
-            if(dx < 0){
-                std::swap(p0,p1);
+                //acá debería tener un while
+            }       
+          
+        } else {
+          if (dx >=0) {
+            m = dx/dy;
+              
+            for( Point p = p0; p.y <= p1.y; ++p.y) {
+                if(dx == 0) {
+                    glVertex2f(p0.x,p0.y);
+                  
+                } 
+                  
+                glVertex2f(p0.x,p0.y);
+                di = 0;
+                while(p.x < p1.x){
+                      
+                    float h = di + m;
+                    if( ((2*dx*di) + (2*dy) - dx) < 0) {
+                        //Pinto E
+                        glVertex2f(p.x+1, p.y);
+                        di += h;
+                        pasoDi = 2*dx*di + 2*dy;  
+                    }else{
+                        //Pinto NE
+                        glVertex2f(p.x+1, p.y+1);
+                        //di += h - 1;
+                        pasoDi = 2*dx*di + 2*dy - 2*dx;
+                    }
+                    
+                    if(di == 0){
+                        glVertex2f(p.x,p.y);
+                        pasoDi = 0;
+                        
+                    }
+                    
+                    di = pasoDi;
+                }
+                  
             }
-        
-            float m = dy/dx;
-    
-            for( Point p = p0; p.x <= p1.x; ++p.x) {
-                glVertex2f(redon(p.x),p.y);
-                p.y+= m;
-            }
-        }
+              
+        }   
     }
+        
+    
+    //DDA
+//    float dy = (p1.y-p0.y);
+//    float dx = (p1.x-p0.x);
+ 
+    //Como se si la curva es suficientemente suave?
+    
+//    if(abs(dx) < abs(dy)) {
+//        if(dy!=0){
+//            if(dy < 0){
+//                std::swap(p0,p1);
+//            }
+//                
+//            float m = dx/dy;
+//            for( Point p = p0; p.y <= p1.y; ++p.y) {
+//                glVertex2f(p.x,redon(p.y));
+//                p.x+= m;
+//            }    
+//        }    
+//    }else{
+//        if(dx!=0){
+//            if(dx < 0){
+//                std::swap(p0,p1);
+//            }
+//        
+//            float m = dy/dx;
+//    
+//            for( Point p = p0; p.x <= p1.x; ++p.x) {
+//                glVertex2f(redon(p.x),p.y);
+//                p.y+= m;
+//            }
+//        }
+//    }
     glEnd();
 }
 
